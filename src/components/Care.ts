619575ffc31705266c14e0d1c60d01a541a37748
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-06 13:35:22 -0800

    Add care component

---
+++ src/components/Care.ts
@@ -0,0 +1,21 @@
+import { Component, BaseComponent, Intents } from '@jovotech/framework';
+
+/*
+|--------------------------------------------------------------------------
+| Component
+|--------------------------------------------------------------------------
+|
+| A component consists of handlers that respond to specific user requests
+| Learn more here: www.jovo.tech/docs/components, jovo.tech/docs/handlers
+|
+*/
+@Component()
+export class Care extends BaseComponent {
+  START() {
+    this.$send('Adventure minigame');
+  }
+
+  UNHANDLED() {
+    return this.START();
+  }
+}
