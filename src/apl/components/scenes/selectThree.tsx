86def9443adc5507c58c3703e4323f0822aeb9d9
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-10 17:02:53 -0800

    More components

--- src/apl/components/scenes/selectThree.tsx
+++ src/apl/components/scenes/selectThree.tsx
@@ -1,6 +1,78 @@
 type Item = {
-  imageUrl: string;
+  img: string;
   name: string;
 };
 
-export const selectThree = (items: Item[]) => {};
+export const selectThree = (items: Item[]) => {
+  const renderItem = (item: Item) => ({
+    type: 'Container',
+    direction: 'row',
+    justifyContent: 'spaceEvenly',
+    items: [
+      {
+        type: 'Container',
+        direction: 'column',
+        alignItems: 'center',
+        items: [
+          {
+            type: 'Image',
+            source: item.img,
+            width: '20vw',
+            height: '20vw',
+          },
+          {
+            type: 'Text',
+            text: item.name,
+            color: '@text',
+          },
+        ],
+      },
+    ],
+  });
+
+  const renderItems = () => items.map(renderItem);
+
+  return {
+    type: 'APL',
+    version: '2023.2',
+    settings: {},
+    import: [
+      {
+        name: 'alexa-layouts',
+        version: '1.7.0',
+      },
+    ],
+    resources: [
+      {
+        colors: {
+          text: '#565A5C',
+        },
+      },
+    ],
+    styles: {},
+    onMount: [],
+    graphics: {},
+    commands: {},
+    layouts: {},
+    mainTemplate: {
+      parameters: ['payload'],
+      items: [
+        {
+          type: 'Frame',
+          backgroundColor: '#fff',
+          items: [
+            {
+              type: 'Container',
+              width: '100vw',
+              height: '100vh',
+              direction: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              items: renderItems(),
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
