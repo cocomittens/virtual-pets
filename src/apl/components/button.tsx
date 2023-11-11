71588e0fddaf6bcaf817647950962556313f784d
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-10 17:42:32 -0800

    Button component

--- src/apl/components/button.tsx
+++ src/apl/components/button.tsx
@@ -1,1 +1,19 @@
-export const button = () => {};
+export const button = () => {
+  return {
+    type: 'TouchWrapper',
+    width: '100dp',
+    height: '50dp',
+    onPress: {
+      type: 'SendEvent',
+      arguments: ['Button was pressed'],
+    },
+    item: {
+      type: 'Text',
+      text: 'Press Me',
+      color: 'white',
+      backgroundColor: 'blue',
+      textAlign: 'center',
+      textAlignVertical: 'center',
+    },
+  };
+};
