f7bd6281c71aec3c4f01732669130a2c06dbb96e
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-11 14:15:17 -0800

    Make results component

---
+++ src/apl/minigames/results.tsx
@@ -0,0 +1,21 @@
+export const Results = () => {
+  return {
+    type: 'APL',
+    version: '1.0',
+    import: [
+      {
+        name: 'alexa-layouts',
+        version: '1.0.0',
+      },
+    ],
+    mainTemplate: {
+      parameters: ['payload'],
+      items: [
+        {
+          type: 'Text',
+          text: 'Results screen',
+        },
+      ],
+    },
+  };
+};
