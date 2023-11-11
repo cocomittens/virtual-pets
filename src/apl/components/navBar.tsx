9e386caef8bb92c5aebaa1baff15e2323904cff8
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-10 19:16:11 -0800

    Render functions to navbar

--- src/apl/components/navBar.tsx
+++ src/apl/components/navBar.tsx
@@ -3,4 +3,37 @@
 // center: title or xp bar
 // right: money
 
-export const navBar = () => {};
+export const navBar = (left?: boolean, mid?: string, right?: boolean) => {
+  const renderLeft = () => {
+    return {
+      type: 'Image',
+      source: 'https://i.imgur.com/8J0z8wz.png',
+      width: '10vw',
+      height: '10vw',
+    };
+  };
+  const renderMid = () => {
+    return {
+      type: 'Text',
+      text: 'Title',
+    };
+  };
+};
+const renderRight = () => {
+  return {
+    type: 'Container',
+    direction: 'row',
+    items: [
+      {
+        type: 'Image',
+        source: 'https://i.imgur.com/8J0z8wz.png',
+        width: '10vw',
+        height: '10vw',
+      },
+      {
+        type: 'Text',
+        text: 'Title',
+      },
+    ],
+  };
+};
