38a15aea970b5529d3dd015aafa2125632529303
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-30 18:29:32 -0800

    Add prev intent and refactor

--- src/components/menu/customize/Customize.ts
+++ src/components/menu/customize/Customize.ts
@@ -12,14 +12,29 @@ import { Component, BaseComponent, Intents } from '@jovotech/framework';
 @Component()
 export class Customize extends BaseComponent {
   START() {
-    (this.$session.$data as { page: number }).page = 0;
+    const items = [{ name: 'item 1' }, { name: 'item 2' }, { name: 'item 3' }, { name: 'item 4' }];
+    const pageSize = 3;
+    const numPages = Math.ceil(items.length / pageSize);
+    (this.$session.$data as { page: number; numPages: number }) = {
+      page: 0,
+      numPages,
+    };
     return this.$send('Customize pet');
+  }
+
+  @Intents(['PreviousIntent'])
+  prevPage() {
+    let page = (this.$session.$data as { page: number }).page;
+    if (page) {
+      (this.$session.$data as { page: number }).page = page - 1;
+    }
   }
 
   @Intents(['NextIntent'])
   nextPage() {
-    let page = (this.$session.$data as { page: number }).page;
-    if (page) {
+    const numPages = (this.$session.$data as { page: number; numPages: number }).numPages;
+    let page = (this.$session.$data as { page: number; numPages: number }).page;
+    if (page < numPages - 1) {
       (this.$session.$data as { page: number }).page = page + 1;
     }
   }
